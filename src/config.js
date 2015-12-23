ab916c5ac5be3c82e9a0b6f9b306ff9d0b791672
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-12-23 11:42:40 +0100

    Added NODE_ENV to providerId

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const PORT = process.env.PORT || 4000;
 export const KINESIS_STREAM_NAME = process.env.KINESIS_STREAM_NAME;
 
 const commit = readFileSync(`${process.cwd()}/COMMIT`);
-export const PROVIDER_ID = `iwwa-write-api::${commit}::${hostname()}`;
+export const PROVIDER_ID = `iwwa-write-api::${commit}::${NODE_ENV}::${hostname()}`;
 
 export const AWS_CLOUDWATCH_GROUP = "iwwa-write-api";
 export const AWS_CLOUDWATCH_STREAM = PROVIDER_ID;
